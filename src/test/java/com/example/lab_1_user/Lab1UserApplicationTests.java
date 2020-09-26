8bef4f88ddd271a3fc3de9cfb8ca26df1700ce0c
Filip Osoianu <filip.osoianu@sens.media>
2020-09-26 14:50:41 +0300

    create project

---
+++ src/test/java/com/example/lab_1_user/Lab1UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_1_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
